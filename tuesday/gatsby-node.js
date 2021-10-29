3fd3a9bab40fc45f3ce5c31e325da6c45d6f5edb
dontpanic92 <lishengqiu.hit@gmail.com>
2021-10-29 09:42:55 +0000

    Update github actions

--- tuesday/gatsby-node.js
+++ tuesday/gatsby-node.js
@@ -1,5 +1,5 @@
 const {articles} = require('./src/articles');
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 exports.createPages = async (
   {actions, graphql},
@@ -9,14 +9,7 @@ exports.createPages = async (
     let o_title = !!articles[title].original_title ? articles[title].original_title : "";
     let o = !!articles[title].origin ? articles[title].origin : "";
     let command = `cd pandoc && bash run_pandoc.sh "${title}" ../content/${root} "../public/${root}.pdf" "${o_title}" "${o}"`;
-    exec(command,(err, stdout, stderr) => {
-      if (err) {
-        console.log(`Cannot execute ${command}: ${err}`);
-        return;
-      }
-
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
+    let result = execSync(command);
+    console.log(result.toString());
   }
 };
